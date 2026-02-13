286eb5e9ba2fbb87c016dc9b14e9b7261c80caca
Razvan Stefan Morar <business@razvanmorar.com>
2026-02-13 16:47:47 +0200

    Move Author to root package

---
+++ src/main/java/com/crud/exampleapp/Author.java
@@ -0,0 +1,50 @@
+package com.crud.exampleapp;
+
+import java.util.Set;
+
+public class Author {
+    private String id;
+    private String name;
+    private Set<String> books;
+
+    public Author() {
+    }
+
+    public Author(String id, String name, Set<String> books) {
+        this.id = id;
+        this.name = name;
+        this.books = books;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<String> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<String> books) {
+        this.books = books;
+    }
+
+    public void addBook(String book) {
+        books.add(book);
+    }
+
+    public void removeBook(String book) {
+        books.remove(book);
+    }
+}
